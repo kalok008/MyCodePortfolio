c0df4991eae259dec548f558d93515c6f3f36b44
Kaloyan Kirilov <82510560+kalok008@users.noreply.github.com>
2024-09-14 16:04:19 +0300

    Create product.cpp

---
+++ Problems from competitions/Group D/National Olympiad/Municipal Round/2024/product.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int count_5(int num) {
+    int count = 0;
+    while (num % 5 == 0){
+        count++;
+        num /= 5;
+    }
+    return count;
+}
+
+int main() {
+    int a, b;
+    cin>>a>>b;
+
+    int count = 0;
+    for (int i = a; i <= b; i++) {
+        count += count_5(i);
+    }
+
+    cout<<count;
+
+    return 0;
+}
