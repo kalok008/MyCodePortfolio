2dc3d7cdadab8a3d645e6fe6cd8c9c7a0333b874
Kaloyan Kirilov <82510560+kalok008@users.noreply.github.com>
2024-09-23 23:26:36 +0300

    Create Maximum_Number_of_Balloons.cpp

---
+++ Algomap.io/Hashmaps & Sets/Maximum_Number_of_Balloons.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        unordered_map<char, int> counter;
+        string balloon = "balloon";
+
+        for(char c : text){
+            if(balloon.find(c) != string::npos){
+                counter[c]++;
+            }
+        }
+
+        if(counter.find('b') == counter.end() || counter.find('a') == counter.end() || counter.find('l') == counter.end() || counter.find('o') == counter.end() || counter.find('n') == counter.end()){
+            return 0;
+        }else{
+            return min({counter['b'], counter['a'], counter['l'] / 2, counter['o'] / 2, counter['n']});
+        }
+    }
+};
