89dc3a60f44931c7248d9d5ead622fa296e908df
Kaloyan Kirilov <82510560+kalok008@users.noreply.github.com>
2024-09-24 22:46:31 +0300

    Create Min_Stack.cpp

---
+++ Algomap.io/Stacks/Min_Stack.cpp
@@ -0,0 +1,32 @@
+class MinStack {
+public:
+    MinStack() {
+
+    }
+
+    void push(int val) {
+        stk.push(val);
+        if(minStk.empty() || val <= minStk.top()){
+            minStk.push(val);
+        }else{
+            minStk.push(minStk.top());
+        }
+    }
+
+    void pop(){
+        stk.pop();
+        minStk.pop();
+    }
+
+    int top(){
+        return stk.top();
+    }
+
+    int getMin(){
+        return minStk.top();
+    }
+
+private:
+    stack<int> stk;
+    stack<int> minStk;
+};
