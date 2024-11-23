9f6584b81d5a2655b55ac19be33758bcced44c87
Kaloyan Kirilov <82510560+kalok008@users.noreply.github.com>
2024-11-23 23:35:59 +0200

    Create 06. Sum of Odd Numbers.cpp

---
+++ SoftUni Courses/C++ Fundamentals - ноември 2024/1. Basic Syntax/06. Sum of Odd Numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  
+  int n;
+  cin>>n;
+  
+  for(int i=1; i<=n; i++){
+	cout<<2*i-1<<endl;
+  }
+  cout<<"Sum: "<<n*n;
+  
+  return 0;
+}
