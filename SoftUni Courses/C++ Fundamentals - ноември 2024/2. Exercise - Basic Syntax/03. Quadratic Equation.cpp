9136f3bab207405cf92cd2781526ac59d9413dd5
Kaloyan Kirilov <82510560+kalok008@users.noreply.github.com>
2024-11-26 23:50:42 +0200

    Create 03. Quadratic Equation.cpp

---
+++ SoftUni Courses/C++ Fundamentals - ноември 2024/2. Exercise - Basic Syntax/03. Quadratic Equation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+  
+  double a, b, c;
+  cin>>a>>b>>c;
+  
+  double D=b*b-4*a*c;
+  
+  if(D<0){
+	cout<<"no roots";
+  }else if(D==0){
+   cout<< (-b + sqrt(D))/(2*a);
+  }else if(D>0){
+   cout<< (-b + sqrt(D))/(2*a)<<' '<<(-b - sqrt(D))/(2*a);
+  }
+  
+ return 0; 
+}
